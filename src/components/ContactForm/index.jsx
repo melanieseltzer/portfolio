627e234c39ae544c499c6cc8b651fe07da3b9c2f
Melanie Seltzer <melleh11@gmail.com>
2018-07-15 20:44:43 -0700

    :bug: Fix contact form and add more styles

--- src/components/ContactForm/index.jsx
+++ src/components/ContactForm/index.jsx
@@ -3,24 +3,19 @@ import { withFormik, Form, Field } from 'formik';
 import * as Yup from 'yup';
 import './style.scss';
 
-const InnerForm = ({ values, errors, touched, isSubmitting }) => (
-  <Form
-    className="form__contact"
-    action="https://formspree.io/hi@melanieseltzer.io"
-    method="POST"
-  >
+const InnerForm = ({ errors, touched, isSubmitting }) => (
+  <Form className="form__contact">
     <Field type="text" name="name" placeholder="Name" />
     {touched.name &&
       errors.name && <span className="errors">{errors.name}</span>}
     <Field type="email" name="email" placeholder="Email" />
     {touched.email &&
       errors.email && <span className="errors">{errors.email}</span>}
-    <textarea name="message" placeholder="Message" />
+    <Field component="textarea" name="message" placeholder="Message" />
     {touched.message &&
       errors.message && <span className="errors">{errors.message}</span>}
-    <button type="submit" disabled={isSubmitting}>
-      Send
-    </button>
+    <button type="submit">{isSubmitting ? 'Sending...' : 'Send'}</button>
+    <span id="messages" />
   </Form>
 );
 
@@ -38,7 +33,42 @@ const ContactForm = withFormik({
       .email('Email not valid')
       .required('Email is required'),
     message: Yup.string().required('Message is required')
-  })
+  }),
+  handleSubmit(values, { resetForm, setSubmitting }) {
+    const url = `https://formcarry.com/s/UBSqyHcclJl`;
+
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(values),
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(() => {
+        resetForm();
+        setSubmitting(false);
+
+        const success = document.getElementById('messages');
+        success.innerHTML = "Success! I'll get back to you ASAP.";
+
+        // Reset message after 5 seconds
+        setTimeout(() => {
+          success.innerHTML = '';
+        }, 5000);
+      })
+      .catch(() => {
+        setSubmitting(false);
+
+        const success = document.getElementById('messages');
+        success.innerHTML = 'Oh no! Something went wrong.';
+
+        // Reset message after 5 seconds
+        setTimeout(() => {
+          success.innerHTML = '';
+        }, 5000);
+      });
+  }
 })(InnerForm);
 
 export default ContactForm;
